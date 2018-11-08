a6372ad120aff9cac16e82634bc0cefb9113760f
serega <ser191097@gmail.com>
2018-11-08 23:23:43 +0300

    pthreads

--- phtread(sspp3.1)/main.cpp
+++ phtread(sspp3.1)/main.cpp
@@ -1,111 +1,100 @@
+
 #include <stdio.h>
+#include <vector>
 #include <fstream>
 #include <iostream>
-
+#include <math.h>
+#include <pthread.h>
 
 using namespace std;
 
+
+
+struct smpl{
+    long long  start;
+    long long end;
+    int size;
+    int rank;
+    int count;
+    clock_t time;
+    std::vector<bool> * prime;
+    smpl(int s = 1 , int r = 1, int c = 0, long long st = 0, long long e = 1, clock_t t = 0, std::vector<bool> *p = NULL){
+        size = s;
+        rank = r;
+        count = c;
+        start = st;
+        end = e;
+        time = t;
+        prime = p;
+    }
+};
+
+void *worker(void *atr) {
+
+    smpl *a = (smpl*) atr;
+    //cout << "worke # " << a->rank << " count: " << a->count << "size: " << a->size << "start: " << a->start << endl;
+    long long  ibegin = (long long) (a->rank - 1)  * (a->end - a->start + 1)/(a->size - 1)  + a->start;
+    long long  iend = (long long ) a->rank  * (a->end - a->start + 1)/(a->size - 1)  + a->start- 1; 
+    std::vector<bool> prime1(iend - ibegin + 1, true);
+    clock_t time_start = clock();
+
+    for(long long  j = 2; j * j <= a->end; ++j) {
+        if((*(*a).prime)[j]) {
+            for(long long  i = (ibegin/j + 1 * (ibegin % j != 0)) * j; i <= min(iend, a->end); i += j) {
+                prime1[i - ibegin] = false;
+            }
+        }
+
+    }
+    clock_t time_finish = clock();
+    for(long long  i = ibegin; i <= min(iend, a->end); ++i) {
+
+        if (prime1[i - ibegin]) {   
+            a->count = a->count + 1;
+        }
+    }
+    a->time = time_finish - time_start;
+    cout << "worke # " << a->rank << " count: " << a->count << " time: " << (double)a->time/CLOCKS_PER_SEC << endl;
+    return a;
+}
+
+
 int main(int argc, char **argv)
 {   
-    int rank, size;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    double time_start, time_finish;
     long long  begin, end;
-    int tagdata = 1;
-    int tagtime = 2;
     sscanf(argv[1], "%llu", &begin);
     sscanf(argv[2], "%llu", &end);
-    vector<bool> prime(end + 1, true);
-    
-    MPI_Status status;
+    int numthread;
+    sscanf(argv[4], "%d", &numthread);
+    //vector<bool> prime(end + 1, true);
+    std::vector<bool> prime((int) sqrt(end + 1.0), true);
     prime[0] = prime[1] = false; 
     long long  temp;
+    int count = 0;
     for(temp = 2; temp * temp <= end; ++temp) {
         if (prime[temp]) {
+            count++;
             for(long long  j = temp * temp; j * j <= end; j += temp ) {
                 prime[j] = false;
             }
         }
 
     }
-    long long  ibegin = (long long) (rank - 1)  * (end - max(begin,temp) + 1)/(size - 1)  + max(begin,temp);
-    long long  iend = (long long ) rank  * (end - max(begin,temp) + 1)/(size - 1)  + max(begin,temp) - 1;  
-    //cout << rank << ' ' << ibegin << ' ' << iend << endl;
-    if (rank != 0) {
 
-        time_start = MPI_Wtime();
-        for(long long  j = 2; j * j <= end; ++j) {
-            if(prime[j]) {
-                for(long long  i = (ibegin/j + 1 * (ibegin % j != 0)) * j; i <= min(iend, end); i += j) {
-                    prime[i] = false;
-                }
-            }
-            
-        }
-        /*for(long long  i = ibegin; i <= min(iend,end); ++i) {
-            cout << rank << ' ' << i << ' ' << prime[i] << endl;
-        }*/
-        for(long long  i = ibegin; i <= min(iend, end); ++i) {
-            if (prime[i]) {   
-                MPI_Send(&i, 1 , MPI_LONG_LONG, 0, tagdata, MPI_COMM_WORLD);
-            }
-        }
-        long long int i = -1;
-        MPI_Send(&i, 1, MPI_LONG_LONG, 0, tagdata, MPI_COMM_WORLD);
-        time_finish = MPI_Wtime();
-        double time = time_finish - time_start;
-        MPI_Send(&time, 1, MPI_DOUBLE, 0, tagtime, MPI_COMM_WORLD);
-    } else {
-        std::vector<long long > simple;
-        long long  count = 0;
-        long long  countfin = 0;
-        std::vector<double> time;
-        int k = 0;
-        while(1) {
-            long long  tmp ;
-                
-            MPI_Recv(&tmp, 1, MPI_LONG_LONG, MPI_ANY_SOURCE, tagdata, MPI_COMM_WORLD, &status);
-            if (tmp != -1) {
-                simple.push_back(tmp);
-                count++;
-            } else  {
-                double tmptime;
-                MPI_Recv(&tmptime, 1, MPI_DOUBLE ,MPI_ANY_SOURCE, tagtime, MPI_COMM_WORLD, &status);
-                time.push_back(tmptime);
-                countfin++;
-                if ( countfin == (size - 1)) {
-                    break;
-                }
-            }
-        }
-        
-        ofstream out;
-        out.open(argv[3]);
-        for(temp = begin; temp * temp <= end; ++temp) {
-            if (prime[temp]) {
-                out << temp << ' ';
-                count++;
-            }
-
-        }
-
-        cout << count << endl;
-        for(long long i = 0; i < count; ++i) {
-            out << simple[i] << '\n';
-        }        
-        double sumtime = 0;
-        double maxtime = 0;
-        for(k = 1; k < size; ++k) {
-            sumtime += time[k-1];
-            if (time[k-1] > maxtime) {
-                maxtime = time[k-1];
-            }
-        }
-        cout << "all time: " << sumtime << endl << "max time: " << maxtime << endl;
+    pthread_t tid[numthread];
+    smpl param[numthread];
+    for(int i = 0 ; i < numthread; ++i) {
+        param[i] = smpl(numthread + 1, i + 1, 0, max(begin, temp), end, 0, &prime);
     }
-    MPI_Finalize();
+    for(int i = 0 ; i < numthread; ++i) {
+        pthread_create(&tid[i], NULL, worker, &param[i]);
+    }
+    for(int i = 0; i < numthread; ++i) {
+        smpl *smtemp;
+        pthread_join(tid[i],(void **) &smtemp);
+        count += smtemp->count;
+    }
+    cout << count << endl;
     return 0;
 }
