64ed80350da07551dcb35bb270823d4a60d0e8ab
serega <ser191097@gmail.com>
2018-11-13 21:51:10 +0300

    pthreads_fin

--- phtread(sspp3.1)/main.cpp
+++ phtread(sspp3.1)/main.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <math.h>
 #include <pthread.h>
+#include <time.h>
+
 
 using namespace std;
 
@@ -16,9 +18,9 @@ struct smpl{
     int size;
     int rank;
     int count;
-    clock_t time;
+    double time;
     std::vector<bool> * prime;
-    smpl(int s = 1 , int r = 1, int c = 0, long long st = 0, long long e = 1, clock_t t = 0, std::vector<bool> *p = NULL){
+    smpl(int s = 1 , int r = 1, int c = 0, long long st = 0, long long e = 1, double t = 0, std::vector<bool> *p = NULL){
         size = s;
         rank = r;
         count = c;
@@ -36,7 +38,9 @@ void *worker(void *atr) {
     long long  ibegin = (long long) (a->rank - 1)  * (a->end - a->start + 1)/(a->size - 1)  + a->start;
     long long  iend = (long long ) a->rank  * (a->end - a->start + 1)/(a->size - 1)  + a->start- 1; 
     std::vector<bool> prime1(iend - ibegin + 1, true);
-    clock_t time_start = clock();
+    timespec time_start;
+    timespec time_finish;
+    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &time_start);
 
     for(long long  j = 2; j * j <= a->end; ++j) {
         if((*(*a).prime)[j]) {
@@ -46,15 +50,16 @@ void *worker(void *atr) {
         }
 
     }
-    clock_t time_finish = clock();
+
+    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &time_finish);
     for(long long  i = ibegin; i <= min(iend, a->end); ++i) {
 
         if (prime1[i - ibegin]) {   
             a->count = a->count + 1;
         }
     }
-    a->time = time_finish - time_start;
-    cout << "worke # " << a->rank << " count: " << a->count << " time: " << (double)a->time/CLOCKS_PER_SEC << endl;
+    a->time = double (time_finish.tv_sec - time_start.tv_sec + 1e-9 * (time_finish.tv_nsec - time_start.tv_nsec));
+    cout << "worke # " << a->rank << " count: " << a->count << " time: " << (double)a->time << endl;
     return a;
 }
 
@@ -88,12 +93,18 @@ int main(int argc, char **argv)
         param[i] = smpl(numthread + 1, i + 1, 0, max(begin, temp), end, 0, &prime);
         pthread_create(&tid[i], NULL, worker, &param[i]);
     }
-    
+    double maxtime = 0;
+    double sumtime = 0;
     for(int i = 0; i < numthread; ++i) {
         smpl *smtemp;
         pthread_join(tid[i],(void **) &smtemp);
         count += smtemp->count;
+        sumtime += smtemp->time;
+        if (smtemp->time > maxtime) {
+        	maxtime = smtemp->time;
+        }
     }
     cout << count << endl;
+    cout << "maxtime: " << maxtime << endl << "sumtime: " << sumtime << endl;
     return 0;
 }
